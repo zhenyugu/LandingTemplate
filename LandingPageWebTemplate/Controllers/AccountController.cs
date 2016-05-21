99ed364ffb9fa90010a46354afe66cf16881a707
Phineas Jiang <Phineas Jiang>
2016-05-21 22:29:58 +0800

    implement base controller.

---
+++ LandingPageWebTemplate/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LandingPageWebTemplate.Controllers
+{
+    public class AccountController : BaseController
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
--- LandingPageWebTemplate/Controllers/BaseController.cs
+++ LandingPageWebTemplate/Controllers/BaseController.cs
@@ -1,15 +1,47 @@
 ﻿using DataAccessLayer;
 using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Extensions;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace LandingPageWebTemplate.Controllers
 {
     public class BaseController : Controller
     {
-       
+
+        private UserManager<IdentityUser> _userManager;
+
+        private IdentityDbContext<IdentityUser> _dbContext;
+
+        public UserManager<IdentityUser> UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<UserManager<IdentityUser>>();
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
+        public IdentityDbContext<IdentityUser> DbContext
+        {
+            get
+            {
+                return _dbContext ?? HttpContext.GetOwinContext().GetUserManager<CustomizedDbContext>();
+            }
+            set
+            {
+                _dbContext = value;
+            }
+        }
 	}
 }
